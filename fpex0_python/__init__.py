9859240e20eb275f72b4799718a5e90b295b3d07
Michael Strik <michael.strik@stud.uni-heidelberg.de>
2022-10-19 13:57:53 +0200

    switched to flat-layout

--- fpex0_python/__init__.py
+++ fpex0_python/__init__.py
@@ -1,8 +1,3 @@
-"""
-This file is executed when the module is imported (i.e. "import fpex0"). Nothing else.
-That also means that no files are loaded contained in the folder.
-"""
-
 from .fpex0 import *
 from .setup import *
 from .InitialDistribution import *
