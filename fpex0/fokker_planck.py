Fixed sign error in first-order FDs

fpex0/fokker_planck.py
@@ -65,7 +65,7 @@ class FokkerPlanck:
 
             # inner nodes (remember: python slicing is exclusive right)
             i = np.arange(1,N-1)
-            Au[i,:] = ( u[i-1,:] - u[i+1,:] ) / (2*h)            # 1st derivative stencil and scale
+            Au[i,:] = ( u[i+1,:] - u[i-1,:] ) / (2*h)            # 1st derivative stencil and scale
             Bu[i,:] = ( u[i-1,:] - 2*u[i,:] + u[i+1,:] ) / h**2  # 2nd derivative stencil and scale
 
             # last node
@@ -78,7 +78,7 @@ class FokkerPlanck:
 
             # inner nodes (remember: python slicing is exclusive right)
             i = np.arange(1,N-1)
-            Au[i] = ( u[i-1] - u[i+1] ) / (2*h)           # 1st derivative stencil and scale
+            Au[i] = ( u[i+1] - u[i-1] ) / (2*h)           # 1st derivative stencil and scale
             Bu[i] = ( u[i-1] - 2*u[i] + u[i+1] ) / h**2   # 2nd derivative stencil and scale
 
             # last node
@@ -148,7 +148,7 @@ class FokkerPlanck:
             e1 = np.ones(N)
             e0 = np.zeros(N)
             # 1st order stencil
-            self._A =  sparse.dia_matrix(( np.array([e1, e0, -e1]), [-1, 0, 1] ), shape=(N, N))
+            self._A =  sparse.dia_matrix(( np.array([-e1, e0, e1]), [-1, 0, 1] ), shape=(N, N))
             self._A = sparse.csr_matrix(self._A)   # change format to edit single entries
             self._A[0  ,1  ] = 0
             self._A[N-1,N-2] = 0